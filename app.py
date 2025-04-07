017e6c5ba03b247d7a0b4e9ca7d173b8a8f303ba
Victor C Chinyeaka <vctrchinyeaka@gmail.com>
2025-04-07 13:37:16 +0100

    gunicorn

--- app.py
+++ app.py
@@ -143,5 +143,5 @@ if __name__ == '__main__':
                 f.write(src.read())
     
     # Use gunicorn-compatible setup
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 10000))
     app.run(debug=False, host='0.0.0.0', port=port)
